aee07d2d6623e282a6e6c51ebe5df41a21db2707
NiceHatThanks <63918742+NiceHatThanks@users.noreply.github.com>
2026-08-21 14:09:09 +0100

    Add ESP32 captive DNS header

---
+++ firmware/esphome/components/captive_portal/dns_server_esp32_idf.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifdef USE_ESP32
+
+#include "esphome/core/helpers.h"
+#include "esphome/components/network/ip_address.h"
+#include "esphome/components/socket/socket.h"
+
+namespace esphome::captive_portal {
+
+class DNSServer {
+ public:
+  void start(const network::IPAddress &ip);
+  void stop();
+  void process_next_request();
+
+ protected:
+  inline void destroy_socket_() {
+    delete this->socket_;
+    this->socket_ = nullptr;
+  }
+  static constexpr size_t DNS_BUFFER_SIZE = 192;
+
+  socket::ListenSocket *socket_{nullptr};
+  network::IPAddress server_ip_;
+  uint8_t buffer_[DNS_BUFFER_SIZE];
+};
+
+}  // namespace esphome::captive_portal
+
+#endif  // USE_ESP32
