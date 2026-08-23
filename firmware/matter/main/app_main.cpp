99a0330cdf9289a9851b3b4e28d7f9d06759d0ce
NiceHatThanks <63918742+NiceHatThanks@users.noreply.github.com>
2026-08-23 19:16:42 +0100

    feat: wire Button 1 into Matter app

--- firmware/matter/main/app_main.cpp
+++ firmware/matter/main/app_main.cpp
@@ -5,12 +5,13 @@
 #include <esp_matter.h>
 #include <esp_matter_console.h>
 
+#include "scoopy_buttons.hpp"
+#include "scoopy_matter.hpp"
+
 static const char *TAG = "scoopy_matter";
-static uint16_t button_1_endpoint_id = 0;
 
 using namespace esp_matter;
 using namespace esp_matter::attribute;
-using namespace esp_matter::endpoint;
 using namespace chip::app::Clusters;
 
 static void app_event_cb(const ChipDeviceEvent *event, intptr_t arg)
@@ -64,39 +65,6 @@ static esp_err_t app_attribute_update_cb(attribute::callback_type_t type,
     return ESP_OK;
 }
 
-static endpoint_t *create_button_1_endpoint(node_t *node)
-{
-    generic_switch::config_t switch_config;
-    switch_config.switch_cluster.feature_flags =
-        cluster::switch_cluster::feature::momentary_switch::get_id();
-
-    endpoint_t *endpoint = generic_switch::create(node, &switch_config, ENDPOINT_FLAG_NONE, nullptr);
-    if (endpoint == nullptr) {
-        ESP_LOGE(TAG, "Failed to create Button 1 Generic Switch endpoint");
-        return nullptr;
-    }
-
-    cluster_t *switch_cluster = cluster::get(endpoint, Switch::Id);
-    if (switch_cluster == nullptr) {
-        ESP_LOGE(TAG, "Failed to find Switch cluster on Button 1 endpoint");
-        return nullptr;
-    }
-
-    // Match Espressif's standard momentary Generic Switch model. GPIO event
-    // generation is intentionally added only after commissioning is proven.
-    cluster::switch_cluster::feature::action_switch::add(switch_cluster);
-
-    cluster::switch_cluster::feature::momentary_switch_multi_press::config_t multi_press_config;
-    multi_press_config.multi_press_max = 2;
-    cluster::switch_cluster::feature::momentary_switch_multi_press::add(
-        switch_cluster, &multi_press_config);
-
-    button_1_endpoint_id = endpoint::get_id(endpoint);
-    ESP_LOGI(TAG, "Button 1 Generic Switch created on endpoint %u", button_1_endpoint_id);
-
-    return endpoint;
-}
-
 extern "C" void app_main()
 {
     esp_err_t err = nvs_flash_init();
@@ -108,15 +76,14 @@ extern "C" void app_main()
 
     ESP_LOGI(TAG, "Starting experimental Scoopy Matter firmware");
 
-    node::config_t node_config;
-    node_t *node = node::create(&node_config, app_attribute_update_cb, app_identification_cb);
+    node::config_t nodeConfig;
+    node_t *node = node::create(&nodeConfig, app_attribute_update_cb, app_identification_cb);
     if (node == nullptr) {
         ESP_LOGE(TAG, "Failed to create Matter node");
         return;
     }
 
-    endpoint_t *button_1_endpoint = create_button_1_endpoint(node);
-    if (button_1_endpoint == nullptr) {
+    if (!scoopy::matter::createButton1Endpoint(node)) {
         ESP_LOGE(TAG, "Failed to create Button 1 endpoint");
         return;
     }
@@ -134,5 +101,10 @@ extern "C" void app_main()
     esp_matter::console::init();
 #endif
 
-    ESP_LOGI(TAG, "Matter started; Button 1 endpoint is %u", button_1_endpoint_id);
+    if (!scoopy::buttons::initButton1(scoopy::matter::handleButton1Action)) {
+        ESP_LOGE(TAG, "Matter is running, but Button 1 initialization failed");
+        return;
+    }
+
+    ESP_LOGI(TAG, "Matter started; Button 1 endpoint is %u", scoopy::matter::button1EndpointId());
 }
